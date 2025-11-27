2038de0e8cdd6cf7bd4eff677d3511532e111238
DhanrajR007 <dhanrajrathore1111@gmail.com>
2025-11-27 10:03:25 +0530

    feat: Implement initial Three.js galaxy generator with configurable parameters and OrbitControls

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import * as THREE from "three";
 import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
-import { flattenJSON } from "three/src/animation/AnimationUtils.js";
 
+// Debug
+const gui = new dat.GUI();
 // Scene
 const scene = new THREE.Scene();
 
@@ -23,41 +24,84 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 3;
 // parameters
 const parameters = {};
-parameters.count = 1000;
-parameters.size = 0.02;
+parameters.count = 10000;
+parameters.size = 0.01;
 parameters.radius = 5;
 parameters.branches = 3;
 parameters.spin = 1;
 
+let geometry = null;
+let material = null;
+let points = null;
+
 // galaxy
 const generateGalaxy = () => {
+  // destroy old galaxy
+  if (points) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(points);
+  }
+
   // Geometry
-  const geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
   const position = new Float32Array(parameters.count * 3);
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
-    position[i3 + 0] = (Math.random() - 0.5) * 3;
-    position[i3 + 1] = (Math.random() - 0.5) * 3;
-    position[i3 + 2] = (Math.random() - 0.5) * 3;
+    const radius = Math.random() * parameters.radius;
+    const branchAngle =
+      ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    position[i3 + 0] = Math.cos(branchAngle) * radius;
+    position[i3 + 1] = 0;
+    position[i3 + 2] = Math.sin(branchAngle) * radius;
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(position, 3));
 
   // Material
 
-  const material = new THREE.PointsMaterial({
+  material = new THREE.PointsMaterial({
     size: parameters.size,
     sizeAttenuation: true,
     depthWrite: false,
     blending: THREE.AdditiveBlending,
-    color: "blue",
+    color: "yellow",
   });
 
   // Points
 
-  const points = new THREE.Points(geometry, material);
+  points = new THREE.Points(geometry, material);
   scene.add(points);
 };
 generateGalaxy();
+
+// Debug
+
+gui
+  .add(parameters, "count")
+  .min(100)
+  .max(100000)
+  .step(100)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "size")
+  .min(0.001)
+  .max(0.1)
+  .step(0.001)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
+gui.add(parameters, "spin").min(-5).max(5).step(0.001);
 
 scene.add(camera);
 
@@ -74,9 +118,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-
-// Debug
-const gui = new dat.GUI();
 
 // Resize listener
 window.addEventListener("resize", () => {
